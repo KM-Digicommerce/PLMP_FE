d194f3b426171aaaf68c371c4b98123550929dc7
vignesh <vignesh@kmdigicommerce.com>
2024-11-25 11:40:25 +0530

    Updated the navigation

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
-// src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Login from './components/Login'; // Import your Login component
 import HomePage from './components/HomePage'; // Import your HomePage component
-// import ProductDetail from './components/products/ProductDetail';
 
 function App() {
   return (
     <Router>
       <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/Login" element={<Login />} />
-      <Route path="*" element={<Navigate to="/Login" />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="*" element={<Navigate to="/Login" />} /> {/* Redirect unmatched routes */}
         <Route path="/HomePage/*" element={<HomePage />} /> {/* HomePage Route */}
-        {/* <Route path="/product/:productId" element={<ProductDetail />} /> */}
       </Routes>
     </Router>
   );
